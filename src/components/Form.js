3b1a87fe0a7f1417b9b2fbb993e27f8df2fbf2e1
KevinBett <kevinbett458@gmail.com>
2023-05-09 12:11:25 +0300

    completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,14 +13,23 @@ function Form() {
     setLastName(event.target.value);
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
+  // add state for holding error messages
+const [errors, setErrors] = useState([]);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  // first name is required
+  if (firstName.length > 0) {
     const formData = { firstName: firstName, lastName: lastName };
     const dataArray = [...submittedData, formData];
     setSubmittedData(dataArray);
     setFirstName("");
     setLastName("");
+    setErrors([]);
+  } else {
+    setErrors(["First name is required!"]);
   }
+}
 
   const listOfSubmissions = submittedData.map((data, index) => {
     return (
@@ -37,6 +46,14 @@ function Form() {
         <input type="text" onChange={handleLastNameChange} value={lastName} />
         <button type="submit">Submit</button>
       </form>
+      {/* conditionally render error messages */}
+      {errors.length > 0
+        ? errors.map((error, index) => (
+            <p key={index} style={{ color: "red" }}>
+              {error}
+            </p>
+          ))
+        : null}
       <h3>Submissions</h3>
       {listOfSubmissions}
     </div>
